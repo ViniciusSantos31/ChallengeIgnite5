08d6931f5b78173b69b07cd3a76a888f76a5262e
Vinicius Santos <viniciussilveira@mat.ci.ufpb.br>
2022-03-12 10:11:07 -0300

    configure prismic and create a homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next';
 
+import { FaCalendar, FaUser } from 'react-icons/fa';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,9 +26,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+          <img src="/icons/logo_spacetraveling.svg" alt="logo" />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.postContent}>
+            <h1>Como utilizar hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.postInfo}>
+              <div className={styles.postDate}>
+                <FaCalendar /> 15 Mar 2021
+              </div>
+              <div className={styles.postAuthor}>
+                <FaUser />
+                Joseph Oliveira
+              </div>
+            </div>
+          </div>
+          <div className={styles.postContent}>
+            <h1>Como utilizar hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.postInfo}>
+              <div className={styles.postDate}>
+                <FaCalendar /> 15 Mar 2021
+              </div>
+              <div className={styles.postAuthor}>
+                <FaUser />
+                Joseph Oliveira
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.showMore}>
+          <a>Carregar mais posts</a>
+        </div>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
